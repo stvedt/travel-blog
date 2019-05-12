45ccc8a0370461a16a710cb0443e5078b1fd3187
Stephen Tvedt <stvedt@zola.com>
2019-05-12 16:27:09 -0400

    flowing data into and individual post page

--- src/templates/blogPost.js
+++ src/templates/blogPost.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { graphql } from 'gatsby';
 
-const Template = (props) => {
+const Template = ({data}) => {
+  const { markdownRemark } = data;
+  const title = markdownRemark.frontmatter.title;
+  const html = markdownRemark.html;
   return (
-    <div>
-      Blog Post!
-    </div>
-  )
+    <React.Fragment>
+      <h1>{title}</h1>
+      <div className="post-content"
+        dangerouslySetInnerHTML={{__html: html}}
+      />
+    </React.Fragment>
+  );
 }
+
+export const query = graphql`
+  query($pathSlug: String!) {
+    markdownRemark(frontmatter: { path: {eq: $pathSlug} }){
+      html
+      frontmatter {
+        title
+      }
+    }
+  }
+`
 
 export default Template;
